5b3dfb3ce2456c74a75eee87241777643f9442b9
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-30 00:22:54 +0530

    Add files via upload

---
+++ do_while5.js
@@ -0,0 +1,16 @@
+let readlineSync= require("readline-sync")
+var a=readlineSync.question("enter a word==")
+i=a.length-1
+str=" "
+do{
+str=str+a[i]
+i--
+
+if (a==str){
+  console.log("palindrome")
+}
+else{
+  console.log("not palidrome")
+}
+}
+while(i>=0)
