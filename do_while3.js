a64e9ead9bf30679086710ea39b31a57b5154fda
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-30 00:20:36 +0530

    Add files via upload

---
+++ do_while3.js
@@ -0,0 +1,26 @@
+var i=0
+do{
+  console.log(i)
+  i++
+}
+while(i<=10)
+
+
+
+
+                    let i=0
+                    do{
+                        console.log(i)
+                        i++
+                    }
+                    while(i<=10)
+
+
+
+
+const i=0
+do{
+    console.log(i)
+    i++
+}
+while(i<=50)
