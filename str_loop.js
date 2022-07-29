dbe49908634a630b94c0b201cdc97e5015de69fd
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-30 00:47:26 +0530

    Add files via upload

---
+++ str_loop.js
@@ -0,0 +1,14 @@
+var cars ="Aanu";
+let text ="";
+for (let x of cars){
+    text +=x;
+}
+console.log(text)
+
+
+// const cars ="avni";
+// let text ="";
+// for (let x of cars){
+//     text +=x;
+// }
+// console.log(text)
