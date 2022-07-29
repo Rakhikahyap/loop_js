8b9e241f3976dda98a0f73655ec05aa373a05555
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-30 00:45:43 +0530

    Add files via upload

---
+++ patarn2.js
@@ -0,0 +1,20 @@
+// var x=6;
+// var str ="";
+// for (var i=1; i<=x; i++){
+//     for (let k =1; k <=9; k++){
+//         str +=i;
+//     }
+//     str +="\n";
+// }
+// console.log(str);
+
+// const a=3
+// string=""
+// count=0
+// for (const i=0; i>=a; i++){
+//     for (var l=1; j<=10; l++){
+//         string +=i;
+//     }
+//     string +="\n";
+// }
+// console.log(string)
