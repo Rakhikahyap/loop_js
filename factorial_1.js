3c09875e85650024f0cdf17e420ceca086fa0236
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:38:04 +0530

    Add files via upload

---
+++ factorial_1.js
@@ -0,0 +1,19 @@
+// var x=require('readline-sync').questionInt('enter the nuber');
+// var fac=1
+// while(x>0){
+//     fac=fac*x
+//     x=x-1
+// }
+// console.log(fac)
+
+var x =require ('readline-sync').questionInt('enter the numbre');
+var count=0
+var i=0
+while (i<x){
+    i++
+    // let j=0
+    // while (j<i){
+        c++
+    }
+
+console.log(count)
