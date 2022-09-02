5b028cc4bd28ab794685c907601f30f6a1b8a938
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:39:26 +0530

    Add files via upload

---
+++ natural.js
@@ -0,0 +1,32 @@
+// let i = 1, n = 5;
+
+// while loop from i = 1 to 5
+// while (i <= n) {
+//     console.log(i);
+//     i += 1;
+// }
+
+
+// var i =1, x=100;
+// while (i<=x){
+//     console.log(i);
+//     i++
+// }
+
+
+// for (let i = 1; i <= 5; i++) {
+    // break condition     
+//     if (i == 3) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let i=1;
+// while (i<=5){
+//     i++
+// if(i==3){
+//     break;
+// }
+//     console.log(i);
+// }
