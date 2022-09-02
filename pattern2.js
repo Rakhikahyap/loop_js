d4ebfa253c001a34281203dc6f886a339e2d3cd7
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-09-02 22:40:49 +0530

    Add files via upload

---
+++ pattern2.js
@@ -0,0 +1,23 @@
+// a=int(input("enter a number"))
+// i=1
+// while i<=a:
+//     j=1
+//     while j<=10:
+//         print(i,'*',j,'=',i*j)
+//         j=j+1
+  
+
+// let a=require('readline-sync').questionInt('enter the number');
+// // let a=5
+// i=1
+// while (i<=a){
+//     j=1
+//     while (j<=10){
+//         console.log(i*j);
+//         j++;
+//     }
+//     console.log()
+//     i++;
+// }
+
+
